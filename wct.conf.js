e6854a0057069301a83981e1346ae9954e6ce1ef
Todd Medema <toddmedema@gmail.com>
2016-09-05 11:15:39 -0400

    fix testing browser error

--- wct.conf.js
+++ wct.conf.js
@@ -1,10 +1,15 @@
 var path = require('path');
 
-var ret = {
-  'suites': ['app/test'],
-  'webserver': {
-    'pathMappings': []
-  }
+var config = {
+  plugins: {
+    local: {
+      browsers: ['chrome'],
+    }
+  },
+  suites: ['app/test'],
+  webserver: {
+    pathMappings: [],
+  },
 };
 
 var mapping = {};
@@ -13,6 +18,6 @@ var rootPath = (__dirname).split(path.sep).slice(-1)[0];
 mapping['/components/' + rootPath  +
 '/app/bower_components'] = 'bower_components';
 
-ret.webserver.pathMappings.push(mapping);
+config.webserver.pathMappings.push(mapping);
 
-module.exports = ret;
+module.exports = config;
