db5eeb2d0f71876e30d165748d6186ab3418d13e
Todd Medema <toddmedema@gmail.com>
2016-09-05 17:31:33 -0400

    add android build file

---
+++ build_android_signed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Tutorial:
+# http://developer.android.com/tools/publishing/app-signing.html#signing-manually
+
+# To generate a new key:
+# keytool -genkey -v -keystore android-release-key.keystore -alias expedition_android -keyalg RSA -keysize 2048 -validity 10000
+
+rm expedition.apk
+
+# Building the release package
+cordova build --release android
+
+# Signing the release APK
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../android-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk expedition_android
+
+# Verification:
+jarsigner -verify -verbose -certs platforms/android/build/outputs/apk/android-release-unsigned.apk
+
+# Aligning memory blocks (takes less RAM on app)
+../zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk expedition.apk
