8e4fb7b73e4c0e5768c1b8cc5de219f8810d91b0
Todd Medema <ToddMedema@gmail.com>
2016-11-30 10:33:38 -0500

    gets iOS auth working; web app auth still working

--- app/constants.tsx
+++ app/constants.tsx
@@ -2,5 +2,8 @@ export const authSettings = {
   urlBase: "http://quests.expeditiongame.com",
   apiKey: "AIzaSyCgvf8qiaVoPE-F6ZGqX6LzukBftZ6fJr8",
   scopes: "profile",
+  // web:
   clientId: "545484140970-r95j0rmo8q1mefo0pko6l3v6p4s771ul.apps.googleusercontent.com",
+  // iOS (for reference):
+  // clientId: "545484140970-lgcbm3df469kscbngg2iof57muj3p588.apps.googleusercontent.com",
 };
