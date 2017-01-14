d9e20ba6c16adefc9215ab7d3cb60913cba28586
Todd Medema <ToddMedema@gmail.com>
2017-01-13 19:43:52 -0500

    add tutorial quest, bump version

--- app/components/FeaturedQuestsContainer.tsx
+++ app/components/FeaturedQuestsContainer.tsx
@@ -8,6 +8,7 @@ import FeaturedQuests, {FeaturedQuestsStateProps, FeaturedQuestsDispatchProps} f
 const mapStateToProps = (state: AppState, ownProps: FeaturedQuestsStateProps): FeaturedQuestsStateProps => {
   return {
     quests: [
+      {title: 'Learning to Adventure', summary: 'Your first adventure.', publishedurl: 'quests/learning_to_adventure.xml'},
       {title: 'Oust Albanus', summary: 'Your party encounters a smelly situation.', publishedurl: 'quests/oust_albanus.xml'},
       {title: 'Mistress Malaise', summary: 'Mystery, Misfortune, and a Mistress.', publishedurl: 'quests/mistress_malaise.xml'},
     ]
