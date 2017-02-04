8555a4d099f6749977d71c40b484c391af0d7db9
Todd Medema <toddmedema@gmail.com>
2017-02-04 10:57:09 -0500

    Fix issues created by the latest app updates (#169)
    * bugfix
    
    * update to match app changes
    
    * code review cleanup

--- app/components/AppContainer.tsx
+++ app/components/AppContainer.tsx
@@ -19,18 +19,25 @@ const mapStateToProps = (state: AppState, ownProps: any): AppStateProps => {
 const mapDispatchToProps = (dispatch: Redux.Dispatch<any>, ownProps: any): AppDispatchProps => {
   return {
     playFromCursor: (editor: EditorState, quest: QuestType) => {
-      var newNode = renderXML(quest.mdRealtime.getText()).getResultAt(editor.line);
-      var tag = newNode.get(0).tagName;
+      const xml = renderXML(quest.mdRealtime.getText());
+      const questNode = xml.getResult();
+      const targetNode = xml.getResultAt(editor.line);
+      const tag = targetNode.get(0).tagName;
       if (tag === 'roleplay' || tag === 'combat') {
         dispatch({type: 'REBOOT_APP'});
-        dispatch(toCard('QUEST_START'));
-        loadNode({numPlayers: 1, difficulty: "NORMAL", showHelp: false, multitouch: false}, dispatch, newNode, defaultQuestContext());
+        dispatch(initQuest(renderXML(quest.mdRealtime.getText()).getResult().children().eq(0), defaultQuestContext()));
+        loadNode({numPlayers: 1, difficulty: "NORMAL", showHelp: false, multitouch: false}, dispatch, targetNode, defaultQuestContext());
+      } else {
+        alert("Invalid cursor position; to play from the cursor, cursor must be on a roleplaying or combat card.");
       }
     },
     playFromStart: (editor: EditorState, quest: QuestType) => {
+      const xml = renderXML(quest.mdRealtime.getText());
+      const questNode = xml.getResult();
+      const targetNode = questNode.children().eq(0);
       dispatch({type: 'REBOOT_APP'});
       dispatch(initQuest(renderXML(quest.mdRealtime.getText()).getResult().children().eq(0), defaultQuestContext()));
-      dispatch(toCard('QUEST_START'));
+      loadNode({numPlayers: 1, difficulty: "NORMAL", showHelp: false, multitouch: false}, dispatch, targetNode, defaultQuestContext());
     }
   };
 }
