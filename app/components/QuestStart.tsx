104c8f8f524c04fb528fdb965feaa5b88c8efc81
Todd Medema <toddmedema@gmail.com>
2017-03-17 09:38:23 -0400

    condense setup to fit on one page (#264)

--- app/components/QuestStart.tsx
+++ app/components/QuestStart.tsx
@@ -20,26 +20,25 @@ const QuestStart = (props: QuestStartProps): JSX.Element => {
   const singlePlayer = (props.settings.numPlayers === 1);
   const multiPlayer = (props.settings.numPlayers > 1);
   return (
-    <Card title="Quest Start">
-      <p>At this time:</p>
+    <Card title="Setup">
       <ol>
-        <li><strong>Select</strong> one Adventurer from the deck{multiPlayer && ' and pass the deck along'}.</li>
-        {singlePlayer && <ul><li><strong>Single Player:</strong> Adventurers with only music abilities are not recommended for solo play.</li></ul>}
-        <li><strong>Clip</strong> a health tracking clip onto your Adventurer card at full health.</li>
+        <li><strong>Select</strong> one adventurer from the deck{multiPlayer && ' and pass the deck along'}.</li>
+        {singlePlayer && <ul><li><strong>Single Player:</strong> Adventurers with music abilities are not recommended for solo play.</li></ul>}
+        <li><strong>Clip</strong> a health tracker onto your adventurer at full health.</li>
         <li><strong>Keep</strong> this card face up in front of you.</li>
-        <li><strong>Draw</strong> the starting abilities listed on the Adventurer card.</li>
-        {singlePlayer && <ul><li><strong>Single Player:</strong> When drawing each ability, you may draw three and pick one.</li></ul>}
+        <li><strong>Draw</strong> the starting abilities listed on your adventurer.</li>
+        {singlePlayer && <ul><li><strong>Single Player:</strong> When drawing each ability, you may draft pick (draw three, keep one).</li></ul>}
         <li><strong>Read</strong> the drawn cards and understand what they do.</li>
         <li>You may mulligan (redraw all of your abilities) once if desired.</li>
         <li><strong>Shuffle</strong> them into a stack face-down in front of you.</li>
         <li><strong>Get</strong>{multiPlayer && ' at least'} one D20 die.</li>
-        <li><strong>Draw</strong> a <strong>Helper card</strong> for reference.</li>
+        <li><strong>Draw</strong> a <strong>helper card</strong> for reference.</li>
       </ol>
 
-      <p>
+      {multiPlayer && <p>
         During your adventure, pass this device to your right whenever you make
         a decision or hit the "Next" button during the story (not during combat).
-      </p>
+      </p>}
 
       <Button onTouchTap={() => props.onNext(props.node)}>Next</Button>
     </Card>
