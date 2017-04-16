9b69e75782d23700a9cba5dc18834922353f45c0
Todd Medema <ToddMedema@gmail.com>
2017-04-16 11:26:35 -0400

    move Android / passphrase entry to last part of prod deploy

--- prod.sh
+++ prod.sh
@@ -16,15 +16,6 @@ rm platforms/android/build/outputs/apk/expedition.apk
 # Rebuild the web app files
 webpack --config ./webpack.dist.config.js
 
-# Android: build the signed prod app (requires password entry)
-cordova build --release android
-# Signing the release APK
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../android-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk expedition_android
-# Verification:
-jarsigner -verify -verbose -certs platforms/android/build/outputs/apk/android-release-unsigned.apk
-# Aligning memory blocks (takes less RAM on app)
-./zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/expedition.apk
-
 # iOS
 cordova build ios
 
@@ -33,3 +24,13 @@ aws s3 cp www s3://app.expeditiongame.com --recursive
 
 # Invalidate files on cloudfront
 aws cloudfront create-invalidation --distribution-id E24IJ45RK0D6J8 --invalidation-batch file://cloudfront-invalidations-prod.json
+
+# Android: build the signed prod app
+# Last because it requires password entry
+cordova build --release android
+# Signing the release APK
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../android-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk expedition_android
+# Verification:
+jarsigner -verify -verbose -certs platforms/android/build/outputs/apk/android-release-unsigned.apk
+# Aligning memory blocks (takes less RAM on app)
+./zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/expedition.apk
