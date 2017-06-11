968610b5da235380fe186efb6ad7ce049d64d5d1
Scott Martin <smartin015@gmail.com>
2017-06-11 08:59:59 -0400

    RM Tutorial

--- app/components/AdvancedPlayContainer.test.tsx
+++ app/components/AdvancedPlayContainer.test.tsx
@@ -9,6 +9,7 @@ describe('AdvancedPlayContainer', () => {
   it('dispatches custom combat on callback', () => {
     const store = mockStore({});
     mapDispatchToProps(store.dispatch, null).onCustomCombatSelect(initial_state);
+    // TODO: Simplify/remove.
     expect(store.getActions()[0]).toEqual(jasmine.objectContaining({
       type: 'NAVIGATE',
       to: jasmine.objectContaining({name:'QUEST_CARD', phase: 'DRAW_ENEMIES'}),
