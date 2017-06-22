99e17053e7ddd3f8afb6193325acb640c4673c9a
Scott Martin <smartin015@gmail.com>
2017-06-22 06:57:21 -0400

    Update err 432

---
+++ errors/definitions/432.tsx
@@ -0,0 +1,23 @@
+export const NAME = `Detected a state where this card has 0 active choices`;
+export const DESCRIPTION = `Some path to this card results in no defined choices.`;
+
+export const INVALID = [
+`_A card_
+
+* {{false}} A single disabled choice
+
+  **end**`
+];
+
+export const VALID = [
+`_A card_
+
+* A single enabled choice
+
+  **end**`,
+`_A card_
+
+_Another card_
+
+**end**`
+];
