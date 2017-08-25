8c2f136e7c17a1c18d79ab41d3671e976e23138c
Todd Medema <toddmedema@gmail.com>
2017-08-25 09:14:32 -0400

    add title too long error (#344)

---
+++ errors/definitions/433.tsx
@@ -0,0 +1,14 @@
+export const NAME = `Card title too long`;
+export const DESCRIPTION = `Card titles may not be 25 characters or longer.`;
+
+export const INVALID = [
+`_A really really long card title_
+
+This will overflow in the app`
+];
+
+export const VALID = [
+`_A short card title_
+
+This will not overflow`
+];
