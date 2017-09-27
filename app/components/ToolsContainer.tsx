3b109988a4617de18d6fe2265d644f874b67d358
Todd Medema <toddmedema@gmail.com>
2017-09-26 21:17:36 -0400

    bugfix: do not allow private searches if user is defined but empty

--- app/components/ToolsContainer.tsx
+++ app/components/ToolsContainer.tsx
@@ -30,7 +30,7 @@ export const mapDispatchToProps = (dispatch: Redux.Dispatch<any>, ownProps: any)
         dispatch(search({owner: u.id, partition: 'expedition-private', expansions: ['horror']}));
       };
 
-      if (!user) {
+      if (!user || user.id === '') {
         dispatch(login(privateSearch));
       } else {
         privateSearch(user);
