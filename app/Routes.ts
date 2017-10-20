4e016010b021b11b5061290f3f21600420823c57
Scott Martin <smartin015@gmail.com>
2017-10-20 17:54:36 -0400

    add port options

--- app/Routes.ts
+++ app/Routes.ts
@@ -25,7 +25,7 @@ const limitCors = Cors({
   // - file (for mobile apps)
   // - localhost (for local dev)
   // - *.local (for dev on mobile)
-  origin: /(expedition(game|rpg)\.com$)|(^file:\/\/)|(localhost(:[0-9]+)?$)|(.*\.local$)/i,
+  origin: /(expedition(game|rpg)\.com$)|(^file:\/\/)|(localhost(:[0-9]+)?$)|(.*\.local(:[0-9]+)?$)/i,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 });
 
