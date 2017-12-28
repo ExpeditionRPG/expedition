440106fa50e2af9eb04e9a3d8e2221e176ab71c3
Todd Medema <toddmedema@gmail.com>
2017-12-28 12:14:12 -0800

    prevent rare audio bug

--- app/components/base/Audio.tsx
+++ app/components/base/Audio.tsx
@@ -365,7 +365,7 @@ export default class Audio extends React.Component<AudioProps, {}> {
     const fadeSeconds = (this.intensity > 0) ? MUSIC_FADE_SECONDS : MUSIC_FADE_LONG_SECONDS;
     for (let i = 0; i < this.musicNodes.length; i++) {
       const track = this.musicNodes[i];
-      if (track.source.playbackState === track.source.PLAYING_STATE) {
+      if (track && track.source && track.source.playbackState === track.source.PLAYING_STATE) {
         track.gain.gain.linearRampToValueAtTime(0, this.ctx.currentTime + fadeSeconds); // Fade out
         track.source.stop = track.source.stop || track.source.noteOff; // polyfill for old browsers
         track.source.stop(this.ctx.currentTime + fadeSeconds);
