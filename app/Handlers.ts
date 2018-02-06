c6d4f21bbfae0bb2a695680c50ab172df3edd43e
Todd Medema <toddmedema@gmail.com>
2018-02-06 08:03:10 -0500

    remove announcement

--- app/Handlers.ts
+++ app/Handlers.ts
@@ -13,8 +13,8 @@ export function healthCheck(req: express.Request, res: express.Response) {
 
 export function announcement(req: express.Request, res: express.Response) {
   res.json({
-    message: 'The Horror is now available!',
-    link: 'https://expeditiongame.com/store/horror?utm_source=app',
+    message: '',
+    link: '',
   });
 }
 
