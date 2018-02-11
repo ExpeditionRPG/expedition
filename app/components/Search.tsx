a995eb0c83ea83f8a78b134d1eb0cb80c931b404
Todd Medema <toddmedema@gmail.com>
2018-02-11 17:14:37 -0500

    fix display of maximum playtime (#585)

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -180,7 +180,15 @@ function renderSettings(props: SearchProps): JSX.Element {
 }
 
 export function formatPlayPeriod(minMinutes: number, maxMinutes: number): string {
-  if (minMinutes >= 60 && maxMinutes >= 60) {
+  if (maxMinutes >= 999) {
+    if (minMinutes >= 999) {
+      return '2+ hrs';
+    } else if (minMinutes >= 60) {
+      return Math.round(minMinutes / 60) + '+ hrs';
+    } else {
+      return Math.round(minMinutes) + '+ min';
+    }
+  } else if (minMinutes >= 60 && maxMinutes >= 60) {
     return Math.round(minMinutes / 60) + '-' + Math.round(maxMinutes / 60) + ' hrs';
   } else {
     return minMinutes + '-' + maxMinutes + ' min';
