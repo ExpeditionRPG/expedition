64a835cc5d28eda6f1b566dd05322acc10caa403
Todd Medema <toddmedema@gmail.com>
2018-04-17 09:37:12 -0400

    1.15.2 + env webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,7 @@ const options = {
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'dev'),
         'API_HOST': JSON.stringify(process.env.API_HOST || 'http://betaapi.expeditiongame.com'),
+        'OAUTH2_CLIENT_ID': JSON.stringify(process.env.OAUTH2_CLIENT_ID || '545484140970-jq9jp7gdqdugil9qoapuualmkupigpdl.apps.googleusercontent.com'),
       },
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/), // Don't import bloated Moment locales
