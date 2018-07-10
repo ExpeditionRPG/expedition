e18453655f72fcc0f14f4bac95b478c2605c4edf
Todd Medema <toddmedema@gmail.com>
2018-07-09 21:43:13 -0400

    consolidate webpack files (#357)
    * consolidate webpack files
    
    * fix tests

---
+++ shared/webpack.dist.shared.js
@@ -0,0 +1,43 @@
+// This config is run to compile and export the production environment to the dist/ folder.
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Webpack = require('webpack');
+const shared = require('./webpack.shared');
+
+const options = {
+  mode: 'production',
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json', '.txt'],
+  },
+  output: {
+    // This must be an absolute path, and thus must be defined per-service
+    // path: 'dist',
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+      ...shared.module.rules,
+    ],
+  },
+  plugins: [
+    new Webpack.optimize.AggressiveMergingPlugin(),
+    new Webpack.DefinePlugin({
+      // Default to beta for safety
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'dev'),
+      'process.env.API_HOST': JSON.stringify(process.env.API_HOST || 'http://betaapi.expeditiongame.com'),
+      'process.env.OAUTH2_CLIENT_ID': JSON.stringify(process.env.OAUTH2_CLIENT_ID || '545484140970-jq9jp7gdqdugil9qoapuualmkupigpdl.apps.googleusercontent.com'),
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/index.html' },
+    ]),
+  ],
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
+};
+
+module.exports = options;
