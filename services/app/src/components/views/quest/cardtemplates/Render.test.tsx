80767a5006ac4e7ad9182b7ab2a375029ac8fa05
Scott Martin <smartin015@gmail.com>
2018-07-31 20:02:33 -0400

    Add unit tests for Render.tsx, plus stubs for Combat.tsx view

--- services/app/src/components/views/quest/cardtemplates/Render.test.tsx
+++ services/app/src/components/views/quest/cardtemplates/Render.test.tsx
@@ -1,10 +1,44 @@
+import {configure, shallow} from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+import * as React from 'react';
+configure({ adapter: new Adapter() });
+import {generateIconElements} from './Render';
+
 describe('Render', () => {
   describe('generateIconElements', () => {
-    it('makes half-size svg for [art] tags');
-    it('makes full-size svg for [art_full] tags');
-    it('makes half-size png for [art_png] tags');
-    it('makes full-size png for [art_full_png] tags');
-    it('makes inline icon for :icon:');
-    it('makes inline white icon for :icon_white: when theme is dark');
+    // NOTE: Since we're dangerously setting inner html, we can't
+    // use enzyme in a more principled way (e.g. finding the img element,
+    // ensuring it exists, that the class is set appropriately and the src attr is
+    // properly set).
+    it('makes half-size svg for [art] tags', () => {
+      const result = shallow(generateIconElements('[art]', 'light')).html();
+      expect(result).toContain('artHalf');
+      expect(result).toContain('art.svg');
+    });
+    it('makes full-size svg for [art_full] tags', () => {
+      const result = shallow(generateIconElements('[art_full]', 'light')).html();
+      expect(result).toContain('artFull');
+      expect(result).toContain('art.svg');
+    });
+    it('makes half-size png for [art_png] tags', () => {
+      const result = shallow(generateIconElements('[art_png]', 'light')).html();
+      expect(result).toContain('artHalf');
+      expect(result).toContain('art.png');
+    });
+    it('makes full-size png for [art_png_full] tags', () => {
+      const result = shallow(generateIconElements('[art_png_full]', 'light')).html();
+      expect(result).toContain('artFull');
+      expect(result).toContain('art.png');
+    });
+    it('makes inline icon for :icon:', () => {
+      const result = shallow(generateIconElements(':icon:', 'light')).html();
+      expect(result).toContain('inline_icon');
+      expect(result).toContain('icon_small.svg');
+    });
+    it('makes inline white icon for :icon_white: when theme is dark', () => {
+      const result = shallow(generateIconElements(':icon_white:', 'dark')).html();
+      expect(result).toContain('inline_icon');
+      expect(result).toContain('icon_white_small.svg');
+    });
   });
 });
--- services/app/src/components/views/quest/cardtemplates/combat/Combat.test.tsx
+++ services/app/src/components/views/quest/cardtemplates/combat/Combat.test.tsx
@@ -1,15 +1,55 @@
 describe('Combat', () => {
-  describe('renderDrawEnemies', () => {
+  describe('DRAW_ENEMIES', () => {
     it('renders all enemies in props');
   });
 
-  describe('renderResolve', () => {
-    it('renders rolls if they are enabled in settings');
+  describe('NO_TIMER', () => {
+    it('shows non-timer prep card');
   });
 
-  describe('renderDefeat', () => {
-    it('Does not show Retry button if onLose does not go to **end**');
-    it('Shows Retry button if onLose goes to **end**');
-    it('Hitting Retry returns the state to the card before combat');
+  describe('PREPARE', () => {
+    it('shows preparation card');
+  });
+
+  describe('TIMER', () => {
+    it('shows timer card');
+  });
+
+  describe('SURGE', () => {
+    it('shows surge card');
+  });
+
+  describe('RESOLVE_ABILITIES', () => {
+    it('shows horror persona helper');
+    it('shows rolls if enabled in settings');
+  });
+
+  describe('RESOLVE_DAMAGE', () => {
+    it('starts at current player and tier count');
+  });
+
+  describe('VICTORY', () => {
+    it('shows a victory page');
+    it('shows healing if not suppressed');
+    it('shows loot if not suppressed');
+    it('shows levelup if not suppressed');
+  });
+
+  describe('DEFEAT', () => {
+    it('does not show Retry button if onLose does not go to **end**');
+    it('shows Retry button if onLose goes to **end**');
+    it('hitting Retry returns the state to the card before combat');
+  });
+
+  describe('MID_COMBAT_ROLEPLAY', () => {
+    it('shows the current parsernode in a dark theme');
+  });
+
+  describe('MID_COMBAT_DECISION', () => {
+    it('shows a Decision element when no scenario chosen');
+    it('shows a Decision element when outcome=retry');
+    it('shows success page on outcome=success');
+    it('shows failure page on outcome=failure');
+    it('shows interrupted page on outcome=interrupted');
   });
 });
