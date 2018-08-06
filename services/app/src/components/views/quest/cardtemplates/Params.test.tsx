dfdcfae6b2cee0c2dcb8dd6e62119cd964fa65ee
Scott Martin <smartin015@gmail.com>
2018-08-06 19:26:08 -0400

    Actually add Params.test.tsx file

---
+++ services/app/src/components/views/quest/cardtemplates/Params.test.tsx
@@ -0,0 +1,8 @@
+describe('Card template params', () => {
+  describe('resolveParams', () => {
+    it('extracts node, decision, and combat');
+    it('resolves node from storage if node not given');
+    it('handles undefined combat state');
+    it('handles undefined decision state');
+  });
+});
