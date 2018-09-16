3640f7b7ed61c7d69b689be19250e312cf592694
Scott Martin <smartin015@gmail.com>
2018-09-16 17:19:45 -0400

    skill check help text, improved Resolve card (#465)
    * Added skill check help text and improved success counter on resolution card
    
    * code review, fixed tests

--- services/app/src/components/views/quest/cardtemplates/decision/ResolveDecision.tsx
+++ services/app/src/components/views/quest/cardtemplates/decision/ResolveDecision.tsx
@@ -1,6 +1,7 @@
 import Button from 'app/components/base/Button';
 import Callout from 'app/components/base/Callout';
 import Card from 'app/components/base/Card';
+import {CardThemeType} from 'app/reducers/StateTypes';
 import * as React from 'react';
 import {Outcome} from 'shared/schema/templates/Decision';
 import {ParserNode} from '../TemplateTypes';
@@ -16,6 +17,20 @@ export interface DispatchProps {
 }
 
 export interface Props extends StateProps, DispatchProps {}
+
+function formatImg(img: string, theme: CardThemeType, small?: boolean) {
+  if (theme === 'dark') {
+    img += '_white';
+  }
+  if (small === undefined || small) {
+    img += '_small';
+  }
+  return img;
+}
+
+function capitalize(text?: string = '') {
+  return text.charAt(0).toUpperCase() + text.substr(1);
+}
 
 export default function resolveDecision(props: Props): JSX.Element {
   const decision = extractDecision(props.node);
@@ -37,7 +52,7 @@ export default function resolveDecision(props: Props): JSX.Element {
     return (
       <Card title={title} theme={props.theme} inQuest={true}>
         {(instruction)
-          ? <Callout icon="adventurer_white">{instruction}</Callout>
+          ? <Callout icon={formatImg('adventurer', props.theme, false)}>{instruction}</Callout>
           : <p>Nothing happens.</p>
         }
         <Button onClick={() => props.onCombatDecisionEnd()}>Next</Button>
@@ -45,17 +60,47 @@ export default function resolveDecision(props: Props): JSX.Element {
     );
   }
 
-  const roll = <img className="inline_icon" src="images/roll_white_small.svg"></img>;
+  const prevSuccesses = computeSuccesses(decision.rolls.slice(0, decision.rolls.length - 1), selected);
   const successes = computeSuccesses(decision.rolls, selected);
+  let helpText: JSX.Element = <span></span>;
+  if (props.settings.showHelp) {
+    helpText = (<span>
+      <h2>Roll <img className="inline_icon" src={'images/' + formatImg('roll', props.theme) + '.svg'}></img></h2>
+      <p>
+        <strong>Roll</strong> for a single adventurer above zero health that hasn't yet rolled.
+        This counts as a roleplaying roll.
+      </p>
+      <h2>Add <img className="inline_icon" src={'images/' + formatImg('skill', props.theme) + '.svg'}></img></h2>
+      <p>
+        <strong>Add</strong> the level of each of the adventurer's {selected.skill} skills.
+        {selected.persona && ` Also add 1 if the adventurer's persona type is ${selected.persona}.`}
+      </p>
+      <h2>Resolve <img className="inline_icon" src={'images/' + formatImg('damage', props.theme) + '.svg'}></img></h2>
+      <p>
+        <strong>Tap</strong> the button matching your roll.
+      </p>
+      <p>
+        <strong>Careful!</strong> Some actions are riskier than others,
+        <br/>and rolling too low may cause instant failure.
+      </p>
+    </span>);
+  }
+
+  const numLeft = selected.requiredSuccesses - successes;
+  const roll = <img className="inline_icon" src={'images/' + formatImg('roll', props.theme) + '.svg'}></img>;
   return (
-    <Card title={(outcome === Outcome.retry) ? 'Keep going!' : 'Resolve Check'} inQuest={true} theme={props.theme}>
-      <p className="center">
-        <strong>{selected.difficulty} {selected.persona} {selected.skill}</strong>
-      </p>
-      <p className="center">
-        ({selected.requiredSuccesses - successes} {pluralize('Success', successes)} Needed)
-      </p>
+    <Card title={[capitalize(selected.persona), capitalize(selected.skill), 'Check'].filter((s) => s).join(' ')} inQuest={true} theme={props.theme}>
+      {helpText}
       {inst}
+      <h2 className="center">
+        {(decision.rolls.length > 0)
+          ? ((successes - prevSuccesses <= 0)
+            ? `Failed; ${numLeft} ${pluralize('Success', numLeft)} Needed`
+            : `Success! ${numLeft} More Needed`
+          )
+          : `${numLeft} ${pluralize('Success', numLeft)} Needed`
+        }
+      </h2>
       <span>
         <Button onClick={() => props.onRoll(props.node, 18)}>{roll} ≥ 17</Button>
         <Button onClick={() => props.onRoll(props.node, 14)}>{roll} 13 - 16</Button>
