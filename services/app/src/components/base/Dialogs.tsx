ca19fb8c0af1f65be528df901cce455ce6391369
Scott Martin <smartin015@gmail.com>
2018-09-22 19:14:29 -0400

    Test dialogs (incl. refactor) (#476)
    * Dialog tests complete
    
    * fix types/build
    
    * fix smol bug

--- services/app/src/components/base/Dialogs.tsx
+++ services/app/src/components/base/Dialogs.tsx
@@ -12,49 +12,86 @@ import {ContentSetsType, DialogState, FeedbackType, QuestState, SavedQuestMeta, 
 import Checkbox from './Checkbox';
 import Picker from './Picker';
 
-interface ExitQuestDialogProps extends React.Props<any> {
+export interface BaseDialogProps extends React.Props<any> {
   onClose: () => void;
-  onExitQuest: () => void;
   open: boolean;
 }
+export class ConfirmationDialog<T extends BaseDialogProps> extends React.Component<T, {}> {
+  protected title: string;
+  protected content: JSX.Element|null;
+  protected action?: string;
 
-export class ExitQuestDialog extends React.Component<ExitQuestDialogProps, {}> {
+  constructor(props: T) {
+    super(props);
+  }
+
+  public onAction() {
+    throw new Error('Unimplemented');
+  }
+
   public render(): JSX.Element {
     return (
       <Dialog classes={{paperWidthSm: 'dialog'}} open={Boolean(this.props.open)}>
-        <DialogTitle>Exit quest?</DialogTitle>
+        <DialogTitle>{this.title || 'Confirm'}</DialogTitle>
         <DialogContent className="dialog">
-          <p>Tapping exit will lose your place in the quest and return you to the home screen.</p>
+          {this.content}
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => this.props.onClose()}>Cancel</Button>
-          <Button className="primary" onClick={() => this.props.onExitQuest()}>Exit</Button>
+          <Button id="closeButton" onClick={() => this.props.onClose()}>Cancel</Button>
+          <Button id="actionButton" className="primary" onClick={() => this.onAction()}>{this.action || 'Exit'}</Button>
         </DialogActions>
       </Dialog>
     );
   }
 }
 
-interface ExitMultiplayerDialogProps extends React.Props<any> {
-  onClose: () => void;
-  onExitMultiplayer: () => void;
-  open: boolean;
+interface ExitDialogProps extends BaseDialogProps {
+  onExit: () => void;
 }
 
-export class ExitMultiplayerDialog extends React.Component<ExitMultiplayerDialogProps, {}> {
-  public render(): JSX.Element {
-    return (
-      <Dialog classes={{paperWidthSm: 'dialog'}} open={Boolean(this.props.open)}>
-        <DialogTitle>Exit Multiplayer?</DialogTitle>
-        <DialogContent className="dialog">
-          <p>Tapping exit will disconnect you from your peers and return you to the home screen.</p>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => this.props.onClose()}>Cancel</Button>,
-          <Button className="primary" onClick={() => this.props.onExitMultiplayer()}>Exit</Button>
-        </DialogActions>
-      </Dialog>
-    );
+export class ExitQuestDialog extends ConfirmationDialog<ExitDialogProps> {
+  constructor(props: ExitDialogProps) {
+    super(props);
+    this.title = 'Exit Quest?';
+    this.content = <p>Tapping exit will lose your place in the quest and return you to the home screen.</p>;
+  }
+
+  public onAction() {
+    this.props.onExit();
+  }
+}
+
+export class ExitMultiplayerDialog extends ConfirmationDialog<ExitDialogProps> {
+  constructor(props: ExitDialogProps) {
+    super(props);
+    this.title = 'Exit Multiplayer?';
+    this.content = <p>Tapping exit will disconnect you from your peers and return you to the home screen.</p>;
+  }
+
+  public onAction() {
+    this.props.onExit();
+  }
+}
+
+interface DeleteSavedQuestDialogProps extends BaseDialogProps {
+  onDelete: (savedQuest: SavedQuestMeta) => void;
+  savedQuest: SavedQuestMeta|null;
+}
+
+export class DeleteSavedQuestDialog extends ConfirmationDialog<DeleteSavedQuestDialogProps> {
+  constructor(props: DeleteSavedQuestDialogProps) {
+    super(props);
+    this.title = 'Delete saved quest?';
+    this.content = null;
+    this.action = 'Delete';
+  }
+
+  public onAction() {
+    const savedQuest = this.props.savedQuest;
+    if (savedQuest === null) {
+      throw new Error('missing saved quest information');
+    }
+    this.props.onDelete(savedQuest);
   }
 }
 
@@ -90,7 +127,7 @@ export class MultiplayerStatusDialog extends React.Component<MultiplayerStatusDi
         </DialogContent>
         <DialogActions>
           <Button onClick={() => this.props.onClose()}>Cancel</Button>,
-          <Button className="primary" onClick={() => this.props.onSendReport(this.props.user, this.props.questDetails, this.props.stats)}>Send Report</Button>
+          <Button id="sendReportButton" className="primary" onClick={() => this.props.onSendReport(this.props.user, this.props.questDetails, this.props.stats)}>Send Report</Button>
         </DialogActions>
       </Dialog>
     );
@@ -108,13 +145,13 @@ export class ExpansionSelectDialog extends React.Component<ExpansionSelectDialog
       <Dialog classes={{paperWidthSm: 'dialog'}} open={Boolean(this.props.open)}>
         <DialogTitle>Choose game</DialogTitle>
         <DialogContent className="dialog">
-          <Button className="primary large" onClick={() => this.props.onExpansionSelect({horror: false, future: false})}>Base Game</Button>
+          <Button id="base" className="primary large" onClick={() => this.props.onExpansionSelect({horror: false, future: false})}>Base Game</Button>
           <br/>
           <br/>
-          <Button className="primary large" onClick={() => this.props.onExpansionSelect({horror: true})}>Base + Horror</Button>
+          <Button id="horror" className="primary large" onClick={() => this.props.onExpansionSelect({horror: true})}>Base + Horror</Button>
           <br/>
           <br/>
-          <Button className="primary large" onClick={() => this.props.onExpansionSelect({horror: true, future: true})}>Base + Horror + Future</Button>
+          <Button id="future" className="primary large" onClick={() => this.props.onExpansionSelect({horror: true, future: true})}>Base + Horror + Future</Button>
           <p style={{textAlign: 'center', marginTop: '1.5em'}}>This will only appear once, but you can change it at any time in Settings.</p>
           <p style={{textAlign: 'center', marginTop: '1.5em'}}>Don't have the cards? <strong><a href="#" onClick={() => openWindow('https://expeditiongame.com/store?utm_source=app')}>Get a copy</a></strong>.</p>
         </DialogContent>
@@ -123,15 +160,7 @@ export class ExpansionSelectDialog extends React.Component<ExpansionSelectDialog
   }
 }
 
-interface TextAreaDialogProps extends React.Props<any> {
-  onClose: () => void;
-  onFeedbackSubmit: (type: FeedbackType, quest: QuestState, settings: SettingsType, user: UserState, text: string) => void;
-  open: boolean;
-  quest: QuestState;
-  settings: SettingsType;
-  user: UserState;
-}
-class TextAreaDialog<T extends TextAreaDialogProps> extends React.Component<T, {}> {
+export class TextAreaDialog<T extends BaseDialogProps> extends React.Component<T, {}> {
   protected title: string;
   protected content: JSX.Element;
   protected helperText: string;
@@ -166,16 +195,23 @@ class TextAreaDialog<T extends TextAreaDialogProps> extends React.Component<T, {
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => this.props.onClose()}>Cancel</Button>
-          <Button className="primary" onClick={() => this.onSubmit()}>Submit</Button>
+          <Button id="cancelButton" onClick={() => this.props.onClose()}>Cancel</Button>
+          <Button id="submitButton" className="primary" onClick={() => this.onSubmit()}>Submit</Button>
         </DialogActions>
       </Dialog>
     );
   }
 }
 
-export class FeedbackDialog extends TextAreaDialog<TextAreaDialogProps> {
-  constructor(props: TextAreaDialogProps) {
+interface FeedbackDialogProps extends BaseDialogProps {
+  quest: QuestState;
+  settings: SettingsType;
+  user: UserState;
+  onFeedbackSubmit: (type: FeedbackType, quest: QuestState, settings: SettingsType, user: UserState, text: string) => void;
+}
+
+export class FeedbackDialog extends TextAreaDialog<FeedbackDialogProps> {
+  constructor(props: FeedbackDialogProps) {
     super(props);
     this.title = 'Send Feedback';
     this.content = <p>Thank you for taking the time to give us feedback! If you've encountered a bug, please include the steps that you can take to reproduce the issue.</p>;
@@ -187,7 +223,7 @@ export class FeedbackDialog extends TextAreaDialog<TextAreaDialogProps> {
   }
 }
 
-interface ReportErrorDialogProps extends TextAreaDialogProps {
+interface ReportErrorDialogProps extends FeedbackDialogProps {
   error: string;
 }
 export class ReportErrorDialog extends TextAreaDialog<ReportErrorDialogProps> {
@@ -203,13 +239,13 @@ export class ReportErrorDialog extends TextAreaDialog<ReportErrorDialogProps> {
   }
 }
 
-export class ReportQuestDialog extends TextAreaDialog<TextAreaDialogProps> {
-  constructor(props: TextAreaDialogProps) {
+export class ReportQuestDialog extends TextAreaDialog<FeedbackDialogProps> {
+  constructor(props: FeedbackDialogProps) {
     super(props);
     this.title = 'Report Quest';
     this.content = (
       <span>
-        <p>You're reporting an issue with <i>{this.props.quest.details.title}</i>.</p>
+        <p>You're reporting an issue with <i>{props.quest.details.title}</i>.</p>
         <p>You should report a quest (instead of reviewing it at the end of the quest) if it is:</p>
         <ul>
           <li>Offensive or inappropriate for the age level it claimed to be.</li>
@@ -227,13 +263,13 @@ export class ReportQuestDialog extends TextAreaDialog<TextAreaDialogProps> {
 }
 
 interface SetPlayerCountDialogProps extends React.Props<any> {
+  open: boolean;
+  quest: Quest;
+  settings: SettingsType;
   onClose: () => void;
   onMultitouchChange: (v: boolean) => void;
   onPlayerDelta: (numPlayers: number, delta: number) => void;
-  open: boolean;
   playQuest: (quest: Quest) => void;
-  quest: Quest;
-  settings: SettingsType;
 }
 
 export class SetPlayerCountDialog extends React.Component<SetPlayerCountDialogProps, {}> {
@@ -245,25 +281,23 @@ export class SetPlayerCountDialog extends React.Component<SetPlayerCountDialogPr
         this.props.settings.numPlayers <= quest.maxplayers);
     }
     let contents = <div>
-        <Picker label="Adventurers" value={this.props.settings.numPlayers} onDelta={(i: number) => this.props.onPlayerDelta(this.props.settings.numPlayers, i)}>
+        <Picker id="adventurerCount" label="Adventurers" value={this.props.settings.numPlayers} onDelta={(i: number) => this.props.onPlayerDelta(this.props.settings.numPlayers, i)}>
           {!playersAllowed && `Quest requires ${quest.minplayers} - ${quest.maxplayers} players.`}
         </Picker>
-        <Checkbox label="Multitouch" value={this.props.settings.multitouch} onChange={this.props.onMultitouchChange}>
+        <Checkbox id="multitouch" label="Multitouch" value={this.props.settings.multitouch} onChange={this.props.onMultitouchChange}>
           {(this.props.settings.multitouch) ? 'All players must hold their finger on the screen to end combat.' : 'A single tap will end combat.'}
         </Checkbox>
       </div>;
-    const futureError = quest.expansionfuture && !this.props.settings.contentSets.future;
-    const horrorError = quest.expansionhorror && !this.props.settings.contentSets.horror;
-    if (futureError) {
+
+    let expansionErr = '';
+    if (quest.expansionhorror && !this.props.settings.contentSets.horror) {
+      expansionErr = 'Horror';
+    } else if (quest.expansionfuture && !this.props.settings.contentSets.future) {
+      expansionErr = 'Future';
+    }
+    if (expansionErr) {
       contents = <div className="error">
-          The Future expansion is required to play this quest.
-          If you have it, make sure to enable it in settings.
-          Otherwise, you can pick up a copy on
-          <a href="#" onClick={() => openWindow('https://expeditiongame.com/store?utm_source=app')}>the Expedition Store</a>.
-        </div>;
-    } else if (horrorError) {
-      contents = <div className="error">
-          The Horror expansion is required to play this quest.
+          The {expansionErr} expansion is required to play this quest.
           If you have it, make sure to enable it in settings.
           Otherwise, you can pick up a copy on
           <a href="#" onClick={() => openWindow('https://expeditiongame.com/store?utm_source=app')}>the Expedition Store</a>.
@@ -271,41 +305,13 @@ export class SetPlayerCountDialog extends React.Component<SetPlayerCountDialogPr
     }
     return (
       <Dialog classes={{paperWidthSm: 'dialog'}} open={Boolean(this.props.open)}>
-        <DialogTitle>{horrorError || futureError ? 'Expansion Required' : 'How many players?'}</DialogTitle>
+        <DialogTitle>{expansionErr ? 'Expansion Required' : 'How many players?'}</DialogTitle>
         <DialogContent className="dialog">
           {contents}
         </DialogContent>
         <DialogActions>
           <Button onClick={() => this.props.onClose()}>Cancel</Button>
-          {!horrorError && <Button disabled={!playersAllowed} className="primary" onClick={() => this.props.playQuest(this.props.quest)}>Play</Button>}
-        </DialogActions>
-      </Dialog>
-    );
-  }
-}
-
-interface DeleteSavedQuestDialogProps extends React.Props<any> {
-  onClose: () => void;
-  onDeleteSavedQuest: (savedQuest: SavedQuestMeta) => void;
-  open: boolean;
-  savedQuest: SavedQuestMeta|null;
-}
-
-export class DeleteSavedQuestDialog extends React.Component<DeleteSavedQuestDialogProps, {}> {
-  public render() {
-    const savedQuest = this.props.savedQuest;
-    return (
-      <Dialog classes={{paperWidthSm: 'dialog'}} open={Boolean(this.props.open)}>
-        <DialogTitle>Delete saved quest?</DialogTitle>
-        <DialogContent className="dialog"></DialogContent>
-        <DialogActions>
-          <Button onClick={() => this.props.onClose()}>Cancel</Button>
-          <Button className="primary" onClick={() => {
-            if (savedQuest === null) {
-              throw new Error('missing saved quest information');
-            }
-            this.props.onDeleteSavedQuest(savedQuest);
-          }}>Delete</Button>
+          {!expansionErr && <Button id="play" disabled={!playersAllowed} className="primary" onClick={() => this.props.playQuest(this.props.quest)}>Play</Button>}
         </DialogActions>
       </Dialog>
     );
@@ -342,12 +348,12 @@ const Dialogs = (props: Props): JSX.Element => {
       <DeleteSavedQuestDialog
         savedQuest={props.selectedSave}
         open={props.dialog && props.dialog.open === 'DELETE_SAVED_QUEST'}
-        onDeleteSavedQuest={props.onDeleteSavedQuest}
+        onDelete={props.onDeleteSavedQuest}
         onClose={props.onClose}
       />
       <ExitQuestDialog
         open={props.dialog && props.dialog.open === 'EXIT_QUEST'}
-        onExitQuest={props.onExitQuest}
+        onExit={props.onExitQuest}
         onClose={props.onClose}
       />
       <ExpansionSelectDialog
@@ -356,7 +362,7 @@ const Dialogs = (props: Props): JSX.Element => {
       />
       <ExitMultiplayerDialog
         open={props.dialog && props.dialog.open === 'EXIT_REMOTE_PLAY'}
-        onExitMultiplayer={props.onExitMultiplayer}
+        onExit={props.onExitMultiplayer}
         onClose={props.onClose}
       />
       <MultiplayerStatusDialog
--- services/app/src/components/base/DialogsContainer.test.tsx
+++ services/app/src/components/base/DialogsContainer.test.tsx
@@ -2,4 +2,7 @@ describe('DialogsContainer', () => {
   test.skip('maps state', () => { /* TODO */ });
 
   test.skip('dispatches dialog change with onClose', () => { /* TODO */ });
+
+  test.skip('onFeedbackSubmit validates input', () => { /* TODO */ });
+  test.skip('onPlayerDelta does nothing when going below min/above max players', () => { /* TODO */ });
 });
