4875cf7c2e1eb6ed3941ca91bb06f3025cb1af95
Todd Medema <toddmedema@gmail.com>
2018-11-04 12:36:19 -0500

    revert app feedback to just be text-based / not have categories

--- services/app/src/components/views/quest/QuestEnd.tsx
+++ services/app/src/components/views/quest/QuestEnd.tsx
@@ -1,9 +1,5 @@
 import Checkbox from '@material-ui/core/Checkbox';
-import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import InputLabel from '@material-ui/core/InputLabel';
-import NativeSelect from '@material-ui/core/NativeSelect';
 import TextField from '@material-ui/core/TextField';
 import * as React from 'react';
 import {CheckoutState, QuestState, SettingsType, UserState} from '../../../reducers/StateTypes';
@@ -69,117 +65,17 @@ export default class QuestEnd extends React.Component<Props, {}> {
     return Boolean(rating) && rating <= QuestEnd.BAD_RATING_THRESHOLD;
   }
 
-  private onChange(field: keyof QuestEndState, value: string) {
-    this.setState({[field]: value});
-    if (field === 'primaryIssue' && value) {
-      this.setState({needsIssue: false});
-    }
-  }
-
   private renderFeedbackForm(): JSX.Element {
     if (!this.rating()) {
       return <span className="nofeedbackform"/>;
     }
 
-    let details: JSX.Element|null = null;
-    if (this.isGoodRating()) {
-      details = (<span>
-        <p>Glad you liked it! What was your favorite part?</p>
-        <FormControl className="selectfield" fullWidth={true}>
-          <InputLabel htmlFor="favoritePart">Select</InputLabel>
-          <NativeSelect
-            inputProps={{
-              id: 'favoritePart',
-            }}
-            onChange={(e: React.ChangeEvent<HTMLSelectElement>, c: React.ReactNode) => this.onChange('favoritePart', e.target.value)}
-            value={this.state.favoritePart}
-          >
-            <option value=""></option>
-            <option value="Humor">Humor</option>
-            <option value="Choices">Choices</option>
-            <option value="Characters">Characters</option>
-            <option value="Setting">Setting</option>
-            <option value="Pacing">Pacing</option>
-            <option value="Plot">Plot</option>
-            <option value="Other">Other</option>
-          </NativeSelect>
-        </FormControl>
-      </span>);
-    } else {
-      let qualifier: JSX.Element|null = null;
-      if (this.state.primaryIssue && this.state.primaryIssue !== 'Other') {
-        qualifier = (
-          <FormControl className="selectfield" fullWidth={true}>
-            <InputLabel htmlFor="issueQualifier">More Details</InputLabel>
-            <NativeSelect
-              inputProps={{
-                id: 'issueQualifier',
-              }}
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>, c: React.ReactNode) => this.onChange('issueQualifier', e.target.value)}
-              value={this.state.issueQualifier}>
-            <option value=""></option>
-            {this.state.primaryIssue === 'Duration' && [
-              <option key={1} value="shorterThanLabeled">shorter than labeled</option>,
-              <option key={2} value="longerThanLabeled">longer than labeled</option>,
-              <option key={3} value="wantShorter">should be shorter</option>,
-              <option key={4} value="wantLonger">should be longer</option>,
-            ]}
-            {this.state.primaryIssue === 'Quality' && [
-              <option key={1} value="grammar">grammar</option>,
-              <option key={2} value="tense">verb tense</option>,
-              <option key={3} value="sp/typo">spelling/typos</option>,
-              <option key={4} value="punctuation/format">punctuation/formatting</option>,
-              <option key={5} value="run-on">run-on sentences</option>,
-            ]}
-            {this.state.primaryIssue === 'Story' && [
-              <option key={1} value="characterDepth">lack of character depth</option>,
-              <option key={2} value="plot">lack of plot depth</option>,
-              <option key={3} value="choice">lack of choices</option>,
-              <option key={4} value="combat">lack of combat</option>,
-            ]}
-            {this.state.primaryIssue === 'Difficulty' && [
-              <option key={1} value="tooEasyForParty">too easy for our party size</option>,
-              <option key={2} value="tooEasy">too easy overall</option>,
-              <option key={3} value="tooHardForParty">too hard for our party size</option>,
-              <option key={4} value="tooHard">too hard overall</option>,
-            ]}
-            <option value="other">something else</option>
-            </NativeSelect>
-          </FormControl>
-        );
-      }
-
-      details = (<span>
-        <p>{(this.isBadRating()) ? 'Please select the primary issue with the quest:' : 'What do you think could be improved?'}</p>
-        <FormControl error={this.state.needsIssue} className="selectfield" fullWidth={true}>
-          <InputLabel htmlFor="primaryIssue">Primary Issue</InputLabel>
-          <NativeSelect
-            id="primaryIssueSelect"
-            inputProps={{
-              id: 'primaryIssue',
-            }}
-            onChange={(e: React.ChangeEvent<HTMLSelectElement>, c: React.ReactNode) => this.onChange('primaryIssue', e.target.value)}
-            value={this.state.primaryIssue}>
-            <option value=""></option>
-            <option value="Duration">Duration</option>
-            <option value="Quality">Quality</option>
-            <option value="Story">Story</option>
-            <option value="Difficulty">Difficulty</option>
-            <option value="Other">Other</option>
-          </NativeSelect>
-        </FormControl>
-        {this.state.needsIssue && <FormHelperText>You must specify an issue before submitting.</FormHelperText>}
-        {qualifier}
-      </span>);
-    }
-
     return (
       <div>
-        {details}
         <TextField
           className="textfield"
           fullWidth={true}
-          label={<span>Anything else?</span>}
+          label={<span>Feedback for the author?</span>}
           multiline={true}
           margin="normal"
           onChange={(e: any) => this.setState({text: e.target.value})}
