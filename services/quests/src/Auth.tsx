3496889cccd6623bf48d3ae3b4ff49cd3b1bd77a
Scott Martin <smartin015@gmail.com>
2018-11-11 10:45:48 -0500

    Use existing ace editor marker accessor, add test to clear markers

--- services/quests/src/Auth.tsx
+++ services/quests/src/Auth.tsx
@@ -1,7 +1,9 @@
 declare var utils: any;
 declare var window: any;
 
-export const realtimeUtils = new utils.RealtimeUtils({
+const utils = utils || undefined;
+
+export const realtimeUtils = utils && new utils.RealtimeUtils({
   clientId: (process && process.env && process.env.OAUTH2_CLIENT_ID) || '',
   scopes: [ // https://developers.google.com/identity/protocols/googlescopes
     'https://www.googleapis.com/auth/drive.install', // ?
@@ -11,14 +13,16 @@ export const realtimeUtils = new utils.RealtimeUtils({
   ],
 });
 
-// Specifically request id_token response
-realtimeUtils.authorizer.authorize = function(onAuthComplete: any, usePopup: any) {
-  this.onAuthComplete = onAuthComplete;
-  // Try with no popups first.
-  window.gapi.auth.authorize({
-    client_id: this.util.clientId,
-    immediate: !usePopup,
-    response_type: 'token id_token',
-    scope: this.util.scopes,
-  }, this.handleAuthResult);
-};
+if (realtimeUtils) {
+  // Specifically request id_token response
+  realtimeUtils.authorizer.authorize = function(onAuthComplete: any, usePopup: any) {
+    this.onAuthComplete = onAuthComplete;
+    // Try with no popups first.
+    window.gapi.auth.authorize({
+      client_id: this.util.clientId,
+      immediate: !usePopup,
+      response_type: 'token id_token',
+      scope: this.util.scopes,
+    }, this.handleAuthResult);
+  };
+}
