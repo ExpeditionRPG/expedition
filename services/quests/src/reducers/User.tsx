d5348fb677fbb8529140263f577cdaae2f2b09da
Saumya29 <saumyatiwari.29@gmail.com>
2018-12-30 23:58:33 +0530

    fix

--- services/quests/src/reducers/User.tsx
+++ services/quests/src/reducers/User.tsx
@@ -1,20 +1,23 @@
 import Redux from 'redux';
-import {SetProfileMetaAction} from '../actions/ActionTypes';
-import {UserState} from './StateTypes';
+import { SetProfileMetaAction } from '../actions/ActionTypes';
+import { UserState } from './StateTypes';
 
 export const loggedOutUser: UserState = {
   displayName: '',
-   email: '',
-   id: '',
-   image: '',
-   loggedIn: false,
-   lootPoints: 0,
+  email: '',
+  id: '',
+  image: '',
+  loggedIn: false,
+  lootPoints: 0,
 };
 
-export function user(state: UserState = loggedOutUser, action: Redux.Action): UserState {
+export function user(
+  state: UserState = loggedOutUser,
+  action: Redux.Action,
+): UserState {
   switch (action.type) {
     case 'SET_PROFILE_META':
-      const profileAction = (action as SetProfileMetaAction);
+      const profileAction = action as SetProfileMetaAction;
       return {
         displayName: profileAction.user.displayName,
         email: profileAction.user.email,
