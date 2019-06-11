ff05639796d98dc3e00ab02750f7b52a0f69b670
Scott Martin <smartin015@gmail.com>
2019-06-11 09:44:47 -0400

    Add drive.file scope when authenticating quest creator

--- services/quests/src/actions/User.tsx
+++ services/quests/src/actions/User.tsx
@@ -16,7 +16,7 @@ export function setProfileMeta(user: UserState): SetProfileMetaAction {
 
 export function loginUser(showPrompt: boolean, quest?: boolean | string): ((dispatch: Redux.Dispatch<any>) => void) {
   return (dispatch: Redux.Dispatch<any>) => {
-    return loginWebBase(window.gapi, AUTH_SETTINGS.API_KEY, AUTH_SETTINGS.CLIENT_ID, AUTH_SETTINGS.SCOPES)
+    return loginWebBase(window.gapi, AUTH_SETTINGS.API_KEY, AUTH_SETTINGS.CLIENT_ID, AUTH_SETTINGS.SCOPES + ' https://www.googleapis.com/auth/drive.file')
       .then((r) => {
         return registerUserAndIdToken(AUTH_SETTINGS.URL_BASE, r);
       })
