bf68c163b6b43a3487b3e1b6e2505ff0a8433641
Todd Medema <toddmedema@gmail.com>
2019-10-08 21:29:49 -0400

    Update Card Creator printing instructions

--- services/cards/src/components/Renderer.tsx
+++ services/cards/src/components/Renderer.tsx
@@ -122,7 +122,7 @@ class Renderer extends React.Component<StateProps, {}> {
                 <ol>
                   <li>Download this PDF and take it to your local print shop.</li>
                   <li>Have it printed on heavy white cardstock (ideally 80-pound or heavier).</li>
-                  <li>Make sure to print double-sided, and to set to document to 100% zoom.</li>
+                  <li>Make sure to print double-sided, flipped on the long edge, at 100% zoom.</li>
                   <li>Cut the cards using a paper cutter. The more precise you are, the easier they'll be to handle later.</li>
                   <li>Secure your cards with a box or rubber band.</li>
                   <li>Accessories: You'll need at least a dozen tracking clips (such as paper clips) and a d20 die.</li>
