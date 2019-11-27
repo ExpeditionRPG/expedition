80e1f1f9aacc5cfca3fdcb65d6e5a1763d303dc1
smartin015 <smartin015@penguin>
2019-11-27 08:26:50 +0100

    Optimistic commit of test file

---
+++ services/app/src/reducers/CommitID.test.tsx
@@ -0,0 +1,10 @@
+describe('CommitID', () => {
+  describe('MULTIPLAYER_COMMIT', () => {
+    test('updates commitID if commit is greater', () => {
+      throw new Error('todo');
+    });
+    test('keeps commitID if existing ID is greater', () => {
+      throw new Error('todo');
+    });
+  });
+});
